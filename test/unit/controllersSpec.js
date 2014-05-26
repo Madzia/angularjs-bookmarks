63a0cc8ae83d00e10f4d554808625961e076925f
mmotel <mateuszmotel@gmail.com>
2014-05-27 01:24:15 +0200

    some tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-describe('PhoneCat controllers', function() {
+describe('App controllers', function() {
 
   beforeEach(function(){
     this.addMatchers({
@@ -11,62 +11,70 @@ describe('PhoneCat controllers', function() {
     });
   });
 
-  beforeEach(module('phonecatApp'));
-  beforeEach(module('phonecatServices'));
+  beforeEach(module('App'));
+  beforeEach(module('appServices'));
+  beforeEach(module('appControllers'));
 
-  describe('PhoneListCtrl', function(){
-    var scope, ctrl, $httpBackend;
+  describe('MainAppCtrl', function(){
+    var scope, ctrl, $httpBackend, cookieStore;
 
     beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
       $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/phones.json').
-          respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+      // $httpBackend.expectGET('api/auth/login/password').
+      //     respond({"auth": true, "login": "login", "password": "password"});
+      // $httpBackend.expectGET('api/verify/login/1').
+      //     respond({"auth": false, "login": "login", "password": "password"});
 
       scope = $rootScope.$new();
-      ctrl = $controller('PhoneListCtrl', {$scope: scope});
+      cookieStore = {
+        'get': function ( key ) {
+          return null;
+        }
+      }
+      ctrl = $controller('MainAppCtrl', {$scope: scope, $cookieStore: cookieStore});
     }));
 
 
-    it('should create "phones" model with 2 phones fetched from xhr', function() {
-      expect(scope.phones).toEqualData([]);
-      $httpBackend.flush();
+    it('should set AuthUser and currentUser to null', function() {
+      // expect(scope.phones).toEqualData([]);
+      // $httpBackend.flush();
 
-      expect(scope.phones).toEqualData(
-          [{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+      expect(scope.AuthUser).toBe(null);
+      expect(scope.currentUser).toBe(null);
     });
 
 
-    it('should set the default value of orderProp model', function() {
-      expect(scope.orderProp).toBe('age');
-    });
+    // it('should set the default value of orderProp model', function() {
+    //   expect(scope.orderProp).toBe('age');
+    // });
   });
 
 
-  describe('PhoneDetailCtrl', function(){
-    var scope, $httpBackend, ctrl,
-        xyzPhoneData = function() {
-          return {
-            name: 'phone xyz',
-                images: ['image/url1.png', 'image/url2.png']
-          }
-        };
-
-
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
-
-      $routeParams.phoneId = 'xyz';
-      scope = $rootScope.$new();
-      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
-    }));
-
-
-    it('should fetch phone detail', function() {
-      expect(scope.phone).toEqualData({});
-      $httpBackend.flush();
-
-      expect(scope.phone).toEqualData(xyzPhoneData());
-    });
-  });
+  // describe('PhoneDetailCtrl', function(){
+  //   var scope, $httpBackend, ctrl,
+  //       xyzPhoneData = function() {
+  //         return {
+  //           name: 'phone xyz',
+  //               images: ['image/url1.png', 'image/url2.png']
+  //         }
+  //       };
+  //
+  //
+  //   beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+  //     $httpBackend = _$httpBackend_;
+  //     $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData());
+  //
+  //     $routeParams.phoneId = 'xyz';
+  //     scope = $rootScope.$new();
+  //     ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+  //   }));
+  //
+  //
+  //   it('should fetch phone detail', function() {
+  //     expect(scope.phone).toEqualData({});
+  //     $httpBackend.flush();
+  //
+  //     expect(scope.phone).toEqualData(xyzPhoneData());
+  //   });
+  // });
 });
